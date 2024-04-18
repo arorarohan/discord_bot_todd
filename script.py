7c28767457776f4d3bae25de399c167d282bc5f4
zohandoggo <arorarohan907@gmail.com>
2024-04-18 14:02:02 +0800

    added new features

--- script.py
+++ script.py
@@ -1,11 +1,19 @@
 import discord
 from discord.ext import commands
-from keys_ids import *
+from discord import FFmpegPCMAudio
+from config import *
+import csv
+
+#define some constants
+MOSTENIRE_PATH = 'mostenire_list.csv'
+SHAME_PATH = 'file_of_shame.txt'
+AUDIO_DIRECTORY = {'whisper':'todd_whispers.mp3'}
+TARGETED_USER = 'dobesquiddy'
 
 #set our intents
 intents = discord.Intents.all()
 intents.members = True
-
+intents.message_content = True
 
 #initialize the client, set the prefix for bot commands to be 'todd '.
 client = commands.Bot(command_prefix='todd ', intents=intents)
@@ -35,6 +43,24 @@ async def on_member_join(member):
     print("Welcomed a new member!")
 
 
+#this next event will detect when squid says literally anything, and store it in a text file
+@client.event
+async def on_message(message):
+    #check if the message was sent by our targeted user
+    if message.author.name == TARGETED_USER:
+        
+        #save the contents in our file
+        with open(SHAME_PATH,'a') as file:
+            file.write(message.content + '\n')
+        
+        #say that it has been saved and do the poopoo
+        await message.channel.send('poopoo. saved your message in the file of shame. Use <todd file_of_shame> to view.')
+        print(f"message {message.content} saved in {SHAME_PATH}")
+    
+    #without this block, the bot will not do anything with any message if it doesn't execute the above.
+    else:
+        await client.process_commands(message)
+
 #say something when a member is removed
 @client.event
 async def on_member_remove(member):
@@ -46,7 +72,7 @@ async def on_member_remove(member):
 
 
 #############################################################################################################
-################################################COMMANDS#####################################################
+################################################TEXT COMMANDS################################################
 #############################################################################################################
 
 #this decorator will be used for any commands we want to define. The user can call the command by typing 'Todd hi'
@@ -56,11 +82,165 @@ async def hi(ctx):
     await ctx.send("Hello, I am todd, todd the dog.")
     print("greeted someone!")
 
-#mostenire command
+#mostenire command to print the list of mostenires from our csv file
 @client.command()
-async def mostenire(ctx):
-    await ctx.send("mostar")
-    print("spelled mostenire correctly!")
+async def mostenire_list(ctx):
+    message_list = []
+    message = ''
+
+    with open(MOSTENIRE_PATH,'r') as file:
+        reader = csv.reader(file)
+        #add every line in the file to a list
+        for line in reader:
+            message_list.append(line[0])
+        
+    #deal with the case that there could be no lines!
+    if len(message_list) == 0:
+        await ctx.send("There are currently no mostenires in your list.")
+        print("returned an empty list of mostenires!")
+
+    else:
+        #add every item in the list to a string
+        message = "\n".join(message_list)
+        #send the string as a message
+        await ctx.send(message)
+        print("sent a string of mostenires!")
+
+
+#mostenire add command to add mostenires to the list
+@client.command()
+async def mostenire_add(ctx,arg=''):
+    #don't accept an empty argument
+    if not len(arg) > 0:
+        await ctx.send("Please try again with an item to add, ex. <todd mostenire_add mositor>")
+        print('failed to add to mostenire list as there was nothing to add!')
+    else:
+        #open the csv
+        with open(MOSTENIRE_PATH,'a',newline='') as file:
+            writer = csv.writer(file)
+
+            #add the entry
+            writer.writerow([arg])
+        
+        #inform the user and host of our success
+        await ctx.send(f"Added {arg} to the list of mostenires! todd thanks you for your service.")
+        print(f"added {arg} to {MOSTENIRE_PATH}")
+
+
+#command to view the file of shame
+@client.command()
+async def file_of_shame(ctx):
+    #get the contents of the file
+    with open(SHAME_PATH,'r') as file:
+        contents = file.read()
+    
+    #chuck them out if there's anything to chuck.
+    if not contents == '':
+        await ctx.send(f"contents of the file of shame (assigned to user {TARGETED_USER}): \n" + contents)
+        print("printed the file of shame")
+    else:
+        await ctx.send(f"the file of shame is assigned to {TARGETED_USER}, but is empty. todd")
+        print("called an empty file of shame!")
+
+
+##################################################################################################################################
+#######################################################AUDIO COMMANDS#############################################################
+##################################################################################################################################
+
+#join voice channel command
+@client.command(pass_context=True)
+async def join(ctx):
+    #if the user running this command is in a voice channel, join the vc of the user
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        voice = await channel.connect()
+        
+        #inform the user of our success
+        await ctx.send("I am todd. I joined your vc.")
+        print("Joined a voice channel and whispered!")
+    #if the user is not in a voice channel, tell the user we don't know what channel to join!
+    else:
+        await ctx.send("Woof. I can see that you aren't in a voice channel. I'm todd and I'm not very smart. I don't know where to go. I'm todd. Join a voice channel and then try again.")
+        print("explained why we can't join a voice channel!")
+
+
+#whisper on-demand, take arg as the item to play
+@client.command(pass_context=True)
+async def play(ctx,arg=''):
+    #first decide what item to play
+    if arg in AUDIO_DIRECTORY.keys():
+        #play the item
+        voice = ctx.guild.voice_client
+        source = FFmpegPCMAudio(AUDIO_DIRECTORY[arg])
+        
+        #if the bot is not in a vc, we will get an AttributeError
+        try:
+            player = voice.play(source)
+            #inform the user of our success
+            await ctx.send(f"played {arg} to you.")
+            print(f'played {AUDIO_DIRECTORY[arg]} in a voice channel!')
+        except AttributeError:
+            await ctx.send("i am todd and I'm not in a vc. Add me to the one you're in first using <todd join>.")
+
+    #we couldn't match the argument to a file
+    else:
+        #inform the user of our failure
+        await ctx.send("Todd can't play what todd don't know. Use <todd play [item to play]>")
+        print("failed to play a file as there is nothing to play")
+
+
+#pause the playback
+@client.command(pass_context=True)
+async def pause(ctx):
+    #call the voice client for the current server of the channel
+    voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+
+    #check if we are currently playing audio
+    if voice.is_playing():
+        voice.pause()
+        await ctx.send("todd paused playback. todd is me. i am todd.")
+        print("paused playback.")
+    else:
+        await ctx.send("can't pause if i am not playing anything. i am, however, todd")
+        print("failed to pause playback as there is nothing playing.")
+
+
+#resume playback
+@client.command(pass_context=True)
+async def resume(ctx):
+    #like before, get the voice client for the current server of the current channel.
+    voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+
+    #check if we are paused
+    if voice.is_paused():
+        voice.resume()
+        await ctx.send("resumed playback, todd.")
+        print("resumed playback.")
+    else:
+        await ctx.send("todd cannot resume that which does not play in the first place. Woof.")
+
+
+#command to stop playback
+@client.command(pass_context=True)
+async def stop(ctx):
+    #we don't need to do any checks because there won't be an error if nothing to stop.
+    voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+    voice.stop()
+
+
+#command to leave the vc
+@client.command(pass_context=True)
+async def leave(ctx):
+    #if the bot is already in a voice channel, leave it.
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("I am todd. I left the voice channel!")
+        print("Left a voice channel!")
+    #if the bot is not in a voice channel, we can't leave!
+    else:
+        await ctx.send("i am todd. I am not in a voice channel. There isn't anything to leave. I'm not very smart but I know this. todd")
+        print("explained why we couldn't leave a voice channel!")
+
 
 #run the bot, linking it to our token. replace this with a string containing your own token.
 client.run(DISCORD_TOKEN)
