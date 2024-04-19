995a9226a4177b43118c527d398d48eced7086bb
zohandoggo <arorarohan907@gmail.com>
2024-04-19 16:46:51 +0800

    allowed the bot to work without a welcome channel

--- cogs/events.py
+++ cogs/events.py
@@ -12,14 +12,19 @@ class Greetings(commands.Cog):
     #for events, this is our decorator now.
     @commands.Cog.listener()
     async def on_member_join(self, member):
-    
-        #get the channel that we want to send our welcome message in
-        channel = self.client.get_channel(private_config.WELCOME_CHANNEL)
+        
+        #do we have a welcome channel configured?
+        if private_config.WELCOME_CHANNEL is not None:
+            #get the channel that we want to send our welcome message in
+            channel = self.client.get_channel(private_config.WELCOME_CHANNEL)
 
-        #then send our message in that channel, and print that we have done so to the terminal.
-        await channel.send("Yo dog, nice to meet you dog. I'm Todd. the dog. woof bark woof.")
-        print("Welcomed a new member!")
-    
+            #then send our message in that channel, and print that we have done so to the terminal.
+            await channel.send("Yo dog, nice to meet you dog. I'm Todd. the dog. woof bark woof.")
+            print("Welcomed a new member!")
+        
+        #if there's no welcome channel, this method will do nothing.
+        else:
+            pass
 
     #listen for things in each message
     @commands.Cog.listener()
@@ -39,10 +44,17 @@ class Greetings(commands.Cog):
     #say goodbye to people who leave.
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        #once again, first get the channel, then send our message there and tell the host it's been done.
-        channel = self.client.get_channel(private_config.WELCOME_CHANNEL)
-        await channel.send("Goobye. Guess you weren't a real dog. I shouldn't have called you dog. Sincerely, todd the dog (a real dog)")
-        print("Said goobye to a member!")
+        #first check if the welcome channel is configured
+        if private_config.WELCOME_CHANNEL is not None:
+
+            #once again, first get the channel, then send our message there and tell the host it's been done.
+            channel = self.client.get_channel(private_config.WELCOME_CHANNEL)
+            await channel.send("Goobye. Guess you weren't a real dog. I shouldn't have called you dog. Sincerely, todd the dog (a real dog)")
+            print("Said goobye to a member!")
+        
+        #if we don't have a welcome channel
+        else:
+            pass
 
 #this must be present at the end of every cog file to make it work
 async def setup(client):
